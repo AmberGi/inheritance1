868cc41daa15fa7ed4614d6a66c1d625a69e0186
gintarevk <gintare.v.k@gmail.com>
2023-02-15 11:37:02 +0200

    Add new objects into main method.

--- src/main/java/lt/gintare/gitdemo/MainGitDemo.java
+++ src/main/java/lt/gintare/gitdemo/MainGitDemo.java
@@ -1,7 +1,14 @@
 package lt.gintare.gitdemo;
 
+import lt.gintare.inheritance.KlaseA;
+import lt.gintare.inheritance.KlaseB;
+import lt.gintare.inheritance.KlaseC;
+
 public class MainGitDemo {
     public static void main(String[] args) {
 
+        KlaseA klaseA = new KlaseA("");
+        KlaseB klaseB = new KlaseB("", "");
+        KlaseC klaseC = new KlaseC("", "");
     }
 }
